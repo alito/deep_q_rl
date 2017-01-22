c1563b4817b7bb6fb404b5dfba35e23f9413c4e9
Alejandro Dubrovsky <alito@organicrobot.com>
2017-01-22 20:40:06 +1100

    Change run_watch to make it useful. Don't record by default Add option to run nips

--- run_watch.py
+++ run_watch.py
@@ -14,21 +14,30 @@ def run_watch(args):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--no-screen', dest="screen", default=True, action="store_false",
                         help="Don't show the screen. Only option that should come before the network")        
-    parser.add_argument('networkfile', nargs=1,
+    parser.add_argument('--nips', dest="nips", default=False, action="store_true",
+                        help="Use the NIPS network architecture")    
+    parser.add_argument('--record', dest="record", default=False, action="store_true",
+                        help="Record stats for the run")        
+    parser.add_argument('-n', '--network', dest='networkfile', required=True,
                         help='Network file. Use "none" to test a newly created (ie random) network')
     parameters, unknown = parser.parse_known_args(args)
 
-    
-    command = ['./run_double.py', '--steps-per-epoch', '0']
-    if parameters.networkfile[0].lower() != 'none':
-        command.extend(['--nn-file', parameters.networkfile[0]])
+    command = []
+    if parameters.nips:
+        command.append('./run_nips.py')
+    else:
+        command.append('./run_double.py')
+
+    command.extend(['--steps-per-epoch', '0'])
+    if parameters.networkfile.lower() != 'none':
+        command.extend(['--nn-file', parameters.networkfile])
     if parameters.screen:
         command.append('--display-screen')
+    if not parameters.record:
+        command.append("--no-record")
 
     command += unknown
-    p1 = subprocess.Popen(command)
-    
-    p1.wait()
+    subprocess.call(command)
 
     return 0
 
